269125f4e21cbba11694c590abef585fdc10f84f
Pannawit Wantae <97363885+Pannawittt@users.noreply.github.com>
2022-08-09 21:39:37 +0700

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -40,7 +40,7 @@ if __name__=='__main__':
     resnet_model ='CloudClassification_resnet50_v1 (1).pkl'
     predictor_resnet = Predict(resnet_model)
 
-my_team = st.expander(label='Team members')
+my_team = st.write(label='Team members')
 with my_team:
     '[developers] Patompong Oupapong, Pannawit Wantae, Pongsapat Suporn'
     '[advisers] Songkran Buttawong, Suthut Butchanon'
