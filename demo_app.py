a0c198eef73a464653f5c07e17a8cf19c1180ba2
Pannawit Wantae <97363885+pannawittt@users.noreply.github.com>
2022-11-24 15:11:52 +0700

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -2,7 +2,7 @@ import fastai
 #from fastai.vision.widgets import *
 #from fastai.vision.all import *
 from fastai import *
-from fastai.basic_train import load_learner
+from fastai import load_learner
 
 import pathlib
 #pathlib.PosixPath = pathlib.WindowsPath
