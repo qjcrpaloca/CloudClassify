11b67c761eb31821811463fa8d9b6b3ded51c2f2
Pannawit Wantae <97363885+pannawittt@users.noreply.github.com>
2022-12-21 01:18:18 +0700

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -26,7 +26,7 @@ class Predict:
     def get_image_from_upload():
         uploaded_file = st.file_uploader("Upload or Take a Cloud Image",type=['png','jpeg', 'jpg'])
         if uploaded_file is not None:
-            return PILImage.create((uploaded_file))
+            return PILImage.create((uploaded_file, flip=False))
         return None
 
     def display_output(self):
